699170006494536f2bfede7602d4121db8e02d0b
eugene <nikshev81@gmail.com>
2016-09-09 08:27:38 +0300

    Add credentials

--- search.R
+++ search.R
@@ -1,1 +1,14 @@
 #Twitter search script
+library(twitteR);
+#Connect credentials file
+source("credentials.R")
+twitCred <- OAuthFactory$new(consumerKey=consumerKey,
+                             consumerSecret=consumerSecret,
+                             requestURL=reqURL,
+                             accessURL=accessURL,
+                             authURL=authURL)
+download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+
+twitCred$handshake(cainfo="cacert.pem")
+
+registerTwitterOAuth(twitCred)
